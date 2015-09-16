5dac4306cbc418adbdb5787097a4ba57d45e4cf7
Toine Hartman <tjbhartman@gmail.com>
2015-09-16 10:02:42 +0200

    Added epochs and optimized code.

--- assignment1/assignment1.m
+++ assignment1/assignment1.m
@@ -1,10 +1,10 @@
-clear all, clc;
+clear all, clc, clf, close;
 
 % Read features from file
 features = dlmread('features.txt');
 % Read targets from file
-targets_ind = dlmread('targets.txt');
-targets_vec = full(ind2vec(targets_ind', 7));
+targets = dlmread('targets.txt');
+targets_vec = full(ind2vec(targets', 7));
 
 % Define percentage of set used for 
 % [training, testing, validating].
@@ -20,6 +20,7 @@ features_validate = features(divider2 + 1:end, 1:end);
 targets_train = targets_vec(1:end, 1:divider1);
 targets_test = targets_vec(1:end, divider1 + 1:divider2);
 targets_validate = targets_vec(1:end, divider2 + 1:end);
+targets_ind = targets(1:divider1, 1:end);
 
 %Initialize variables
 inputs = 10;
@@ -36,62 +37,92 @@ weights_hidden_output = rand(outputs, hidden_neurons);
 hidden_layer = zeros(hidden_neurons, 1);
 output_layer = zeros(outputs, 1);
 
-% For every product
-for index = 1:size(features_train, 1)
-    %Calculate the values of the hidden layer
-    for i = 1:hidden_neurons
-        for j = 1:inputs
-            temp_value_hidden_layer = temp_value_hidden_layer + weights_input_hidden(i, j) * features_train(index, j) - treshold;
+error = zeros(size(targets_train, 1), outputs);
+error_total = zeros(size(targets_train, 1), 1);
+targets_calc = zeros(size(targets_train, 1), 1);
+
+for iter = 1:250
+    % For every product
+    for index = 1:size(features_train, 1)
+        %Calculate the values of the hidden layer
+        for i = 1:hidden_neurons
+            for j = 1:inputs
+                temp_value_hidden_layer = temp_value_hidden_layer + weights_input_hidden(i, j) * features_train(index, j) - treshold;
+            end
+
+            % Sigmoid
+            current_value = 1 / (1 + exp(-temp_value_hidden_layer));
+            hidden_layer(i, 1) = current_value;
+            temp_value_hidden_layer = 0;
         end
 
-        % Sigmoid
-        current_value = 1 / (1 + exp(-temp_value_hidden_layer));
-        hidden_layer(i, 1) = current_value;
-        temp_value_hidden_layer = 0;
-    end
+        % Calculate the values of the outputs
+        for i = 1:outputs
+            for j = 1:hidden_neurons
+                temp_value_output_layer = temp_value_output_layer + weights_hidden_output(i, j) * hidden_layer(j, 1) - treshold;
+            end
 
-    % Calculate the values of the outputs
-    for i = 1:outputs
-        for j = 1:hidden_neurons
-            temp_value_output_layer = temp_value_output_layer + weights_hidden_output(i, j) * hidden_layer(j, 1) - treshold;
+            % Sigmoid function
+            current_value = 1 / (1 + exp(-temp_value_output_layer));
+            output_layer(i, 1) = current_value;
+            temp_value_output_layer = 0;
         end
 
-        % Sigmoid function
-        current_value = 1 / (1 + exp(-temp_value_output_layer));
-        output_layer(i, 1) = current_value;
-        temp_value_output_layer = 0;
-    end
-
-    error_total = 0;
-    error_deriv = zeros(7, 1);
-    % For every output in outputs, calulate error and adjust weight
-    for k = 1:outputs
-        error = targets_train(1, index) - output_layer(k, 1);
-        error_total = error_total + error;
-        error_deriv(k, 1) = output_layer(k, 1) * (1 - output_layer(k, 1)) * error;
-
-        for j = 1:hidden_neurons
-            weight_delta = learning_rate * hidden_layer(j, 1) * error_deriv;
-            weights_hidden_output(k, j) = weights_hidden_output(k, j) + weight_delta(k, 1);
-        end
-    end
-
-    hidden_error_deriv = zeros(hidden_neurons, 1);
-    % For every hidden neuron, adjust weight for sum(output error)
-    for j = 1:hidden_neurons
-        sum_deriv_weight = 0;
-
+        error_total(index, 1) = 0;
+        error_deriv = zeros(7, 1);
+        % For every output in outputs, calulate error and adjust weight
         for k = 1:outputs
-           sum_deriv_weight = sum_deriv_weight + error_deriv(k, 1) * weights_hidden_output(k, j);
+            error(index, 1) = targets_train(1, index) - output_layer(k, 1);
+            error_total(index, 1) = error_total(index, 1) + error(index, 1);
+            error_deriv(k, 1) = output_layer(k, 1) * (1 - output_layer(k, 1)) * error(index, 1);
+
+            for j = 1:hidden_neurons
+                weight_delta = learning_rate * hidden_layer(j, 1) * error_deriv;
+                weights_hidden_output(k, j) = weights_hidden_output(k, j) + weight_delta(k, 1);
+            end
         end
 
-        hidden_error_deriv(j, 1) = hidden_layer(j, 1) * (1 - hidden_layer(j, 1)) * sum_deriv_weight;
+        hidden_error_deriv = zeros(hidden_neurons, 1);
+        % For every hidden neuron, adjust weight for sum(output error)
+        for j = 1:hidden_neurons
+            sum_deriv_weight = 0;
+
+            for k = 1:outputs
+               sum_deriv_weight = sum_deriv_weight + error_deriv(k, 1) * weights_hidden_output(k, j);
+            end
+
+            hidden_error_deriv(j, 1) = hidden_layer(j, 1) * (1 - hidden_layer(j, 1)) * sum_deriv_weight;
+        end
+
+        for i = 1:inputs
+           for j = 1 : hidden_neurons
+              weights_input_hidden(j, i) = weights_input_hidden(j, i) + learning_rate * features_train(index, i) * hidden_error_deriv(j, 1);
+           end
+        end
+
+        [m, ind] = max(output_layer);
+        targets_calc(index, 1) = ind;
+
     end
 
-    for i = 1:inputs
-       for j = 1 : hidden_neurons
-          weights_input_hidden(j, i) = weights_input_hidden(j, i) + learning_rate * features_train(index, i) * hidden_error_deriv(j, 1);
-       end
+    fprintf('Iteration %d - ', iter);
+    count = 0;
+    for i = 1:size(targets_ind, 1)
+        if targets_ind(i, 1) == targets_calc(i, 1)
+            count = count + 1;
+    %     else
+    %         fprintf('Index %d: given %d, calculated %d\n', i, targets_ind(i), targets_calc(i));
+        end
     end
 
+    p = count / size(targets_train, 2) * 100;
+    fprintf('%d samples, %d correct (%f%%)\n', size(targets_train, 2), count, p);
+
+    result(iter) = p;
 end
+
+figure(1)
+plot(result)
+xlabel('iteration');
+ylabel('% correct');
+title('Neural network');
