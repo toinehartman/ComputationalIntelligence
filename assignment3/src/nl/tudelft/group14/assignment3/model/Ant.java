b0da3ee76cd10b576573ddb873daf993aa6986be
Bram42 <bram.bervoets@gmail.com>
2015-10-22 23:28:51 +0200

    quick fix in move

--- assignment3/src/nl/tudelft/group14/assignment3/model/Ant.java
+++ assignment3/src/nl/tudelft/group14/assignment3/model/Ant.java
@@ -8,21 +8,21 @@ import java.util.Stack;
 
 public class Ant {
 
-    private int x = 0;
-    private int y = 0;
-    private Block currentBlock;
-    private Stack<Block> route;
-    private Set<Block> pheromoneroute;
-    private Maze maze;
-    private Block[][] matrix;
-    private ArrayList<Block> visited;
-    private boolean finished;
-    private float pheromone;
+	private int x = 0;
+	private int y = 0;
+	private Block currentBlock;
+	private Stack<Block> route;
+	private Set<Block> pheromoneroute;
+	private Maze maze;
+	private Block[][] matrix;
+	private ArrayList<Block> visited;
+	private boolean finished;
+	private float pheromone;
 
-    private int[] start;
-    private int[] end;
-    
-    public Stack<Block> getRoute() {
+	private int[] start;
+	private int[] end;
+
+	public Stack<Block> getRoute() {
 		return route;
 	}
 
@@ -42,11 +42,11 @@ public class Ant {
 		currentBlock = matrix[start[0]][start[1]];
 		this.pheromone = pheromone;
 
-        this.start = start;
-        this.end = end;
+		this.start = start;
+		this.end = end;
 	}
-    
-    public boolean isFinished() {
+
+	public boolean isFinished() {
 		return finished;
 	}
 
@@ -55,53 +55,55 @@ public class Ant {
 	}
 
 	public List<Block> cloneNeighbours(List<Block> neighbours) {
-        List<Block> result = new ArrayList<Block>();
-        for (Block b : neighbours)
-            result.add(b);
-        return result;
-    }
-	
-    public void move() {
-        if (!finished) {
-            Block oldCurrBlock = currentBlock;
-            List<Block> currentNeighbours = getNeighbours(currentBlock);
-            currentBlock = decideDirection(currentNeighbours);
-            if (currentBlock == null) {
-                System.out.println(String.format("%s, %s", currentNeighbours.toString(), oldCurrBlock.toString()));
-                System.exit(0);
-            }
-            setX(currentBlock.getX());
-            setY(currentBlock.getY());
-            route.push(currentBlock);
-            visited.add(currentBlock);            
-            pheromoneroute.add(currentBlock);
-            if (x == this.end[0] && y == this.end[1]) {
-                finished = true;
-//                System.out.println(route.toString());
-            }
-        }
-    }
+		List<Block> result = new ArrayList<Block>();
+		for (Block b : neighbours)
+			result.add(b);
+		return result;
+	}
 
-    
+	public void move() {
+		if (!finished) {
+			Block oldCurrBlock = currentBlock;
+			List<Block> currentNeighbours = getNeighbours(currentBlock);
+			currentBlock = decideDirection(currentNeighbours);
+			if (currentBlock == null) {
+				System.out.println(String.format("%s, %s", currentNeighbours.toString(), oldCurrBlock.toString()));
+				System.exit(0);
+			}
+			setX(currentBlock.getX());
+			setY(currentBlock.getY());
+			route.push(currentBlock);
+			visited.add(currentBlock);            
+			pheromoneroute.add(currentBlock);
+			if (x == this.end[0] && y == this.end[1]) {
+				finished = true;
+				//                System.out.println(route.toString());
+			}
+		}
+	}
+
+
 	public List<Block> getNeighbours(Block currentBlock){
 		List<Block> currentNeighbours = maze.getNeighbours(currentBlock);
-		
-		for (Block b : currentNeighbours){			
-			if (visited.size()>1){
-//				System.out.println(b.toString() + "  -  " + visited.get(visited.size()-1));
-				if (b.equals(visited.get(visited.size()-2))){
-//					System.out.println(b.toString());
-					currentNeighbours.remove(b);
+
+		if (currentNeighbours.size() > 1){
+
+			for (Block b : currentNeighbours){			
+				if (visited.size()>1){
+					//				System.out.println(b.toString() + "  -  " + visited.get(visited.size()-1));
+					if (b.equals(visited.get(visited.size()-2))){
+						//					System.out.println(b.toString());
+						currentNeighbours.remove(b);
+					}
 				}
 			}
 		}
-		
 		return currentNeighbours;
-		
+
 	}
-	
-	
-    public Set<Block> getPheromoneroute() {
+
+
+	public Set<Block> getPheromoneroute() {
 		return pheromoneroute;
 	}
 
@@ -110,9 +112,9 @@ public class Ant {
 	}
 
 	public void moveTo(Block block) {
-    	setX(block.getX());
-    	setY(block.getY());
-    }
+		setX(block.getX());
+		setY(block.getY());
+	}
 
 	public void setX(int x) {
 		this.x = x;
@@ -129,33 +131,33 @@ public class Ant {
 	public int getY() {
 		return y;
 	}
-	
+
 	public float getPheromonePerBlock(){
 		float ppb;
-		
+
 		ppb = (this.pheromone / this.route.size());
-		
+
 		return ppb;
-		
+
 	}
 
 	public Floor decideDirection(List<Block> neighbours) {
-    	float totalPheromone = 0;
-    	    	
-    	for (Block b : neighbours) {
-    		Floor f = (Floor)b;
-        	totalPheromone += f.getPheromone();
-        }
-    	
-    	float random = (float)(Math.random());
-    	
-    	for (Block b : neighbours) {
-    		Floor f = (Floor)b;
-    		float percentage = (f.getPheromone() / totalPheromone);
-//    		System.out.println(random + " - " + percentage + "  "  + (random < percentage) + " -  x:" + f.getX() + " y:" + f.getY() + " size: " + neighbours.size());
-    		if ((random -= percentage) < 0 || neighbours.indexOf(b) == neighbours.size() - 1) return f;
-    	}
+		float totalPheromone = 0;
 
-        return null;
-    }
+		for (Block b : neighbours) {
+			Floor f = (Floor)b;
+			totalPheromone += f.getPheromone();
+		}
+
+		float random = (float)(Math.random());
+
+		for (Block b : neighbours) {
+			Floor f = (Floor)b;
+			float percentage = (f.getPheromone() / totalPheromone);
+			//    		System.out.println(random + " - " + percentage + "  "  + (random < percentage) + " -  x:" + f.getX() + " y:" + f.getY() + " size: " + neighbours.size());
+			if ((random -= percentage) < 0 || neighbours.indexOf(b) == neighbours.size() - 1) return f;
+		}
+
+		return null;
+	}
 }
