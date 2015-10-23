9d892e65893e792d89196cdf83590081c8bd8c24
Toine Hartman <tjbhartman@gmail.com>
2015-10-23 12:32:35 +0200

    Fucking gitignore.

---
+++ assignment3/src/nl/tudelft/group14/assignment3/model/Product.java
@@ -0,0 +1,11 @@
+package nl.tudelft.group14.assignment3.model;
+
+/**
+ * Created by toinehartman on 23/10/15.
+ *
+ * @author toinehartman
+ * @version 1.0
+ * @since 23/10/15
+ */
+public class Product {
+}
